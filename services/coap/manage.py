db7099c6d63f242302d454a2edd06d913c3cebe7
Николай Денисов <nickdenisov@protonmail.com>
2022-06-26 17:50:03 +0500

    Added(sevice): coap server

--- services/coap/manage.py
+++ services/coap/manage.py
@@ -1,1 +1,21 @@
-db = DATABASE_URL
+from flask.cli import FlaskGroup
+from flask import Flask
+
+import psycopg2
+
+app = Flask(__name__)
+app.config.from_object("config.Config")
+cli = FlaskGroup(app)
+
+
+def create_db():
+    conn = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
+    cursor = conn.cursor()
+    query_create_sensors_table = "CREATE TABLE sensors (id SERIAL PRIMARY KEY, type VARCHAR(50), location VARCHAR(50));"
+    cursor.execute(query_create_sensors_table)
+    conn.commit()
+    cursor.close()
+
+
+if __name__ == "__main__":
+    create_db()
