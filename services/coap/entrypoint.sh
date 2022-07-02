f1eb9cde46f82974fd11847afe934095d98fb208
Николай Денисов <nickdenisov@protonmail.com>
2022-07-02 15:48:17 +0500

    Исправлено(coap): не работал сервис, так как не мог подключиться к бд

--- services/coap/entrypoint.sh
+++ services/coap/entrypoint.sh
@@ -3,11 +3,12 @@
 echo "Waiting for TimescaleDB to be available..."
 
 # Use netcat (nc) to check if the TimescaleDB host/port are accessible
-while ! nc -z timescale 5432; do
-  sleep 0.1
-done
+#while ! nc -z timescale 5432; do
+#  sleep 0.1
+#done
 
 echo "TimescaleDB started"
 
-python manage.py create_db
+#python manage.py create_db
+python manage.py
 exec "$@"
