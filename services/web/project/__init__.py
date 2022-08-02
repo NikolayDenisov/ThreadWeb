0cdfb4c957dff5ec249bb687619b56b6bd9e8d99
Николай Денисов <nickdenisov@protonmail.com>
2022-08-02 14:48:45 +0500

    Исправлено(веб): отображаем только для определенного eui64

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -25,17 +25,20 @@ def staticfiles(filename):
 
 @app.route("/")
 def hello_world():
-    data = last_day()
+    eui64 = 'F4:CE:36:86:38:EC'
+    data = last_day(eui64)
     timestamps = list(list(zip(*data))[0])
     date_strings = [d.strftime('%d/%m/%Y, %H:%M:%S') for d in timestamps]
     temps = list(list(zip(*data))[1])
     return render_template('chart.html', values=temps, labels=date_strings)
 
 
-def last_day():
+def last_day(eui64):
     query = "SELECT time_bucket('1 hours', time) AS one_hour," \
             "ROUND(AVG(temperature)::numeric, 2) FROM sensor_data " \
-            "WHERE time > now () - INTERVAL '1 day' GROUP BY one_hour ORDER BY one_hour;"
+            "WHERE time > now () - INTERVAL '1 day' AND " \
+            f"eui64='{eui64}'" \
+            "GROUP BY one_hour ORDER BY one_hour;"
     with psycopg2.connect(app.config["SQLALCHEMY_DATABASE_URI"]) as conn:
         cursor = conn.cursor()
         cursor.execute(query)
