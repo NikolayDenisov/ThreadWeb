701251e7967d5ca260f4b2f23f469fc8e1fced05
Николай Денисов <nickdenisov@protonmail.com>
2022-09-06 01:59:28 +0500

    Исправлено(аккаунт): создание пользователей и маршруты

--- services/account/models.py
+++ services/account/models.py
@@ -1,12 +1,38 @@
-from flask_login import UserMixin
+from database import Base
+from flask_security import UserMixin, RoleMixin
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Boolean, DateTime, Column, Integer, \
+    String, ForeignKey, UnicodeText
 
-from . import db
+
+class RolesUsers(Base):
+    __tablename__ = 'roles_users'
+    id = Column(Integer(), primary_key=True)
+    user_id = Column('user_id', Integer(), ForeignKey('user.id'))
+    role_id = Column('role_id', Integer(), ForeignKey('role.id'))
 
 
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
+class Role(Base, RoleMixin):
+    __tablename__ = 'role'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(80), unique=True)
+    description = Column(String(255))
+    permissions = Column(UnicodeText)
 
-    user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+
+class User(Base, UserMixin):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), unique=True)
+    username = Column(String(255), unique=True, nullable=True)
+    password = Column(String(255), nullable=False)
+    last_login_at = Column(DateTime())
+    current_login_at = Column(DateTime())
+    last_login_ip = Column(String(100))
+    current_login_ip = Column(String(100))
+    login_count = Column(Integer)
+    active = Column(Boolean())
+    fs_uniquifier = Column(String(255), unique=True, nullable=False)
+    confirmed_at = Column(DateTime())
+    roles = relationship('Role', secondary='roles_users',
+                         backref=backref('users', lazy='dynamic'))
--- services/account/routes.py
+++ services/account/routes.py
@@ -1,30 +1,29 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
-# from .db import User
-import uuid as uuid_pkg
-from flask_login import login_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
-from . import db
+from flask_security import auth_required, hash_password, utils, login_user, login_required, logout_user
+from models import User
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
+@auth.route('/signin')
+def signin():
+    print('TYTYTY1')
+    return render_template('signin.html')
 
 
-@auth.route('/login', methods=['POST'])
-def login_post():
+@auth.route('/signin', methods=['POST'])
+def signin_post():
+    print('TYTYTYT2')
     email = request.form.get('email')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
-    # user = User.query.filter_by(email=email).first()
-    # if not user and not check_password_hash(user.password, password):
-    #     flash('Неверный логин или пароль')
-    #     return redirect(url_for('auth.login'))
-    # login_user(user, remember=remember)
-    return redirect(url_for('auth.login'))
+    user = User.query.filter_by(email=email).first()
+    print(user)
+    if not user and not utils.verify_password(password, user.password):
+        flash('Неверный логин или пароль')
+        return redirect(url_for('auth.signin'))
+    login_user(user, remember=remember)
+    return redirect(url_for('auth.profile'))
 
 
 @auth.route('/forgot-password')
@@ -32,6 +31,39 @@ def forgot_password():
     return render_template('forgot-password.html')
 
 
-@auth.route('/register')
-def register():
-    return render_template('register.html')
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    # if user:
+    #     flash('Пользователь уже зарегистрирован, авторизуйтесь')
+    #     return redirect(url_for('auth.login'))
+    # db.create_user(email=email, password=hash_password(password))
+    print(request.form, hash_password(password))
+    # return redirect(url_for('account.main'))
+
+
+@auth.route('/profile')
+@auth_required()
+def profile():
+    return render_template('profile.html')
+
+
+@auth.route("/")
+@auth_required()
+def home():
+    return redirect(url_for('auth.profile'))
+
+
+@auth.route('/signout', methods=['GET'])
+@login_required
+def signout():
+    logout_user()
+    return redirect(url_for('auth.signin')) or request.referrer
